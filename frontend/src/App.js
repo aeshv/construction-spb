50958968e0cce1943bb408db85dc7360fe3973b2
Alexey Vlasov <a-vlasov@webjox.ru>
2022-11-29 23:30:46 +0300

    images gallery

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,14 @@ import './App.scss';
 import AboutBlock from './components/AboutBlock/AboutBlock';
 import BenefitsBlock from './components/BenefitsBlock/BenefitsBlock';
 import Footer from './components/Footer/Footer';
+import ImagesGallery from './components/ImagesGallery/ImagesGallery';
 
 function App() {
   return (
     <div className="App">
+      f
+      <ImagesGallery/>
+      ff
       <BenefitsBlock/>
       <AboutBlock/>
       <Footer/>
