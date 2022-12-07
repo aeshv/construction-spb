f5af300f4c1d91e92e7a08f76c6d7f344a05a506
Alexey Vlasov <a-vlasov@webjox.ru>
2022-12-07 22:32:10 +0300

    footer form connect

--- frontend/src/components/BenefitsBlock/BenefitsBlock.jsx
+++ frontend/src/components/BenefitsBlock/BenefitsBlock.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./BenefitsBlock.scss";
 import { useSwipeable } from "react-swipeable";
-import { motion, useInView } from "framer-motion";
+import { AnimatePresence, motion, useInView } from "framer-motion";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import Spacer from "./../UI/Spacer/Spacer";
 
@@ -79,205 +79,164 @@ const BenefitsBlock = () => {
           <div className="swiper__header">{active}/3</div>
           <div className="swiper__body" {...handlersBox}>
             {/* slide 1 */}
-            {active === 1 && (
-              <motion.div
-                className="slide"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.5 }}
-              >
-                <div className="slide__content">
-                  <div className="content">
-                    <div className="content__title">
-                      Собственная база покупателей
-                    </div>
+            <motion.div
+              className="slide"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              <div className="slide__content">
+                <div className="content">
+                  <div className="content__title">
+                    Собственная база покупателей
+                  </div>
+                  {active === 1 && (
+                    <>
+                      <AnimatePresence>
+                        <motion.ul
+                          className="content__list"
+                          initial={{ opacity: 0}}
+                          animate={{ opacity: 1}}
+                          exit={{ opacity: 0}}
+                          transition={{ duration: 0.5 }}
+                        >
+                          <li className="content__item">
+                            <span>
+                              <b>Создаем спрос</b> на объект еще до размещения
+                              его в рекламе
+                            </span>
+                          </li>
+                          <li className="content__item">
+                            <span>
+                              Используем <b>закрытые базы</b> риэлторов
+                            </span>
+                          </li>
+                          <li className="content__item">
+                            <span>
+                              Продаем через собственную наработанную базу из{" "}
+                              <b>6000</b>
+                            </span>
+                          </li>
+                        </motion.ul>
+                      </AnimatePresence>
+                    </>
+                  )}
+                  {active === 2 && (
+                    <>
+                      <AnimatePresence>
+                        <motion.ul
+                          className="content__list"
+                          initial={{ opacity: 0}}
+                          animate={{ opacity: 1}}
+                          exit={{ opacity: 0}}
+                          transition={{ duration: 0.5 }}
+                        >
+                          <li className="content__item">
+                            <span>
+                              <b>Создаем спрос</b> на объект еще до размещения
+                              его в рекламе
+                            </span>
+                          </li>
+                          <li className="content__item">
+                            <span>
+                              Используем <b>закрытые базы</b> риэлторов
+                            </span>
+                          </li>
+                          <li className="content__item">
+                            <span>
+                              Продаем через собственную наработанную базу из{" "}
+                              <b>6000</b>
+                            </span>
+                          </li>
+                        </motion.ul>
+                      </AnimatePresence>
+                    </>
+                  )}
+                  {active === 3 && (
+                    <>
+                      <AnimatePresence>
+                        <motion.ul
+                          className="content__list"
+                          initial={{ opacity: 0}}
+                          animate={{ opacity: 1}}
+                          exit={{ opacity: 0}}
+                          transition={{ duration: 0.5 }}
+                        >
+                          <li className="content__item">
+                            <span>
+                              <b>Создаем спрос</b> на объект еще до размещения
+                              его в рекламе
+                            </span>
+                          </li>
+                          <li className="content__item">
+                            <span>
+                              Используем <b>закрытые базы</b> риэлторов
+                            </span>
+                          </li>
+                          <li className="content__item">
+                            <span>
+                              Продаем через собственную наработанную базу из{" "}
+                              <b>6000</b>
+                            </span>
+                          </li>
+                        </motion.ul>
+                      </AnimatePresence>
+                    </>
+                  )}
 
-                    <motion.ul
-                      className="content__list"
-                      initial={{ opacity: 0, x: -100 }}
-                      animate={{ opacity: 1, x: 0 }}
-                      exit={{ opacity: 0 }}
-                      transition={{ duration: 0.5 }}
+                  <div className="content__control">
+                    <button
+                      className="prev"
+                      onClick={() => changeCurrentActive("minus")}
                     >
-                      <li className="content__item">
-                        <span>
-                          <b>Создаем спрос</b> на объект еще до размещения его в
-                          рекламе
-                        </span>
-                      </li>
-                      <li className="content__item">
-                        <span>
-                          Используем <b>закрытые базы</b> риэлторов
-                        </span>
-                      </li>
-                      <li className="content__item">
-                        <span>
-                          Продаем через собственную наработанную базу из{" "}
-                          <b>6000</b>
-                        </span>
-                      </li>
-                    </motion.ul>
-
-                    <div className="content__control">
-                      <button
-                        className="prev"
-                        onClick={() => changeCurrentActive("minus")}
-                      >
-                        <MdKeyboardArrowLeft />
-                      </button>
-                      <button className="gold">
-                        Получите превью предложения
-                      </button>
-                      <button
-                        className="next"
-                        onClick={() => changeCurrentActive("plus")}
-                      >
-                        <MdKeyboardArrowRight />
-                      </button>
-                    </div>
+                      <MdKeyboardArrowLeft />
+                    </button>
+                    <button className="gold">
+                      Получите превью предложения
+                    </button>
+                    <button
+                      className="next"
+                      onClick={() => changeCurrentActive("plus")}
+                    >
+                      <MdKeyboardArrowRight />
+                    </button>
                   </div>
                 </div>
-                <div className="slide__image">
-                  <img
+              </div>
+              <div className="slide__image">
+                {active === 1 && (
+                  <motion.img
+                    initial={{ opacity: 0.6, y: -100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.5 }}
                     src="http://placeimg.com/900/1000/animals"
                     alt="Здание"
                   />
-                </div>
-              </motion.div>
-            )}
-
-            {/* slide 2 */}
-            {active === 2 && (
-              <motion.div
-                className="slide"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.5 }}
-              >
-                <div className="slide__content">
-                  <div className="content">
-                    <div className="content__title">
-                      2Собственная база покупателей
-                    </div>
-
-                    <motion.ul
-                      className="content__list"
-                      initial={{ opacity: 0, x: -100 }}
-                      animate={{ opacity: 1, x: 0 }}
-                    >
-                      <li className="content__item">
-                        <span>
-                          <b>С2здаем спрос</b> на объект еще до размещения его в
-                          рекламе
-                        </span>
-                      </li>
-                      <li className="content__item">
-                        <span>
-                          Испол2ьзуем <b>закрытые базы</b> риэлторов
-                        </span>
-                      </li>
-                      <li className="content__item">
-                        <span>
-                          Продаем через собственную наработанную базу из{" "}
-                          <b>6000</b>
-                        </span>
-                      </li>
-                    </motion.ul>
-
-                    <div className="content__control">
-                      <button
-                        className="prev"
-                        onClick={() => changeCurrentActive("minus")}
-                      >
-                        <MdKeyboardArrowLeft />
-                      </button>
-                      <button className="gold">
-                        Получите превью предложения
-                      </button>
-                      <button
-                        className="next"
-                        onClick={() => changeCurrentActive("plus")}
-                      >
-                        <MdKeyboardArrowRight />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-                <div className="slide__image">
-                  <img src="http://placeimg.com/900/900/animals" alt="Здание" />
-                </div>
-              </motion.div>
-            )}
-
-            {/* slide 3 */}
-            {active === 3 && (
-              <motion.div
-                className="slide"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.5 }}
-              >
-                <div className="slide__content">
-                  <div className="content">
-                    <div className="content__title">
-                      Собс3твенная база покупателей
-                    </div>
-
-                    <motion.ul
-                      className="content__list"
-                      initial={{ opacity: 0, x: -100 }}
-                      animate={{ opacity: 1, x: 0 }}
-                    >
-                      <li className="content__item">
-                        <span>
-                          <b>Соз3даем спрос</b> на объект еще до размещения его
-                          в рекламе
-                        </span>
-                      </li>
-                      <li className="content__item">
-                        <span>
-                          Испо3льзуем <b>закрытые базы</b> риэлторов
-                        </span>
-                      </li>
-                      <li className="content__item">
-                        <span>
-                          Прода3ем через собственную наработанную базу из{" "}
-                          <b>6000</b>
-                        </span>
-                      </li>
-                    </motion.ul>
-
-                    <div className="content__control">
-                      <button
-                        className="prev"
-                        onClick={() => changeCurrentActive("minus")}
-                      >
-                        <MdKeyboardArrowLeft />
-                      </button>
-                      <button className="gold">
-                        Получите превью предложения
-                      </button>
-                      <button
-                        className="next"
-                        onClick={() => changeCurrentActive("plus")}
-                        style={{ opacity: active === 3 ? "0.2" : "1" }}
-                      >
-                        <MdKeyboardArrowRight />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-                <div className="slide__image">
-                  <img
-                    src="http://placeimg.com/1000/900/animals"
+                )}
+                {active === 2 && (
+                  <motion.img
+                    initial={{ opacity: 0.6, y: -100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.5 }}
+                    src="http://placeimg.com/700/1000/animals"
                     alt="Здание"
                   />
-                </div>
-              </motion.div>
-            )}
+                )}
+                {active === 3 && (
+                  <motion.img
+                    initial={{ opacity: 0.6, y: -100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.5 }}
+                    src="http://placeimg.com/800/900/nature"
+                    alt="Здание"
+                  />
+                )}
+              </div>
+            </motion.div>
           </div>
         </div>
 
