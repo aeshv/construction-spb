95d46666ac7f66938466d22b77d2cc07452feb90
Alexey Vlasov <a-vlasov@webjox.ru>
2022-12-10 14:25:46 +0300

    icons

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import images from "../../assets/images";
 import "./Header.scss";
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { RiTelegramLine } from "react-icons/ri";
+import { SlSocialVkontakte } from 'react-icons/sl'
 
 const Header = () => {
   return (
@@ -9,25 +11,38 @@ const Header = () => {
       <header className="header">
         <div className="header-top">
           <div className="header__social">
-            <div className="header__social--item">VK</div>
-            <div className="header__social--item">TG</div>
-            <div className="header__social--item">WA</div>
+            <div className="header__social--item">
+              <AiOutlineWhatsApp />
+            </div>
+            <div className="header__social--item">
+              <RiTelegramLine />
+            </div>
+            <div className="header__social--item">
+              <SlSocialVkontakte/>
+            </div>
           </div>
 
           <div className="header__contacts">
-            <a className="contacts__email" href="mailto:info@spb-stroimgroup.ru">info@spb-stroimgroup.ru</a>
+            <a
+              className="contacts__email"
+              href="mailto:info@spb-stroimgroup.ru"
+            >
+              info@spb-stroimgroup.ru
+            </a>
             <a className="contacts__number" href="tel:+79099595521">
               <span>Санкт-Петербург</span>
               <a>+7 (909) 959-55-21</a>
             </a>
-            <a href="#contact" className="contacts__button">Заказать Звонок</a>
+            <a href="#contact" className="contacts__button">
+              Заказать Звонок
+            </a>
           </div>
         </div>
 
         <div className="header-bottom">
           <div className="header__logo">
-          {/* <img src={images.logo192} alt="Логотип Компании"/> */}
-          <></>
+            {/* <img src={images.logo192} alt="Логотип Компании"/> */}
+            <></>
           </div>
           <div className="header__menu">
             <nav className="header__list">
